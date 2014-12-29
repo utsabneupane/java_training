6f0e0567c4974a3ca058c9f1eec603f60fc3a8d4
utsabneupane <utsabneupane@lftechnology.com>
2014-12-29 11:29:15 +0545

    "assignment1 on dec26 added"

---
+++ java_training/src/dec26/InitialDemo.java
@@ -0,0 +1,42 @@
+package dec26;
+/*
+ * a program that accepts full name as an input from user and display only its 
+ * initial from that full name
+ */
+
+import java.util.Scanner;
+
+public class InitialDemo{
+
+	private static Scanner sc;			/*initialization of Scanner object for taking input */
+
+	public static void main(String[] args) {
+
+		/* variable to store initial name initialization*/
+		StringBuilder initial=new StringBuilder();
+		sc=new Scanner(System.in);
+		
+		/*input taken from user and stored in variable */
+		System.out.println("Enter your full name: ");
+		String fullName=sc.nextLine();
+		int len=fullName.length();
+		
+		for (int i=0; i<len; i++){
+
+				/* getting character before first dot or space in name*/
+				if( (fullName.charAt(i)=='.') || (fullName.charAt(i)==' ') ){
+					break;
+				}else{
+
+					/*each character before '.' or ' ' is stored as initial*/
+					initial.append(fullName.charAt(i));
+				}
+		}
+		
+		System.out.println("Your initial is: "+initial);
+				
+	/* main function end here*/	
+	}
+/* InitialDemo class ends here*/ 
+}
+
---
+++ java_training/src/dec26/PatternDemo.java
@@ -0,0 +1,54 @@
+package dec26;
+/*
+ * a program that accepts name of cities separated by white space along with character sequence to search city as an input
+ * from user and display the city that falls under that character sequence 
+ * 
+ */
+
+import java.util.Scanner;
+
+public class PatternDemo {
+
+	private static Scanner sc;			/*initialization of Scanner object for taking input */
+
+	public static void main(String[] args) {
+
+		/*variable initialization for cities,pattern, start point, end point*/		
+		String cities;
+		String pattern;
+		int srt=0,end=0;
+		sc=new Scanner(System.in);
+
+		/*takes input from user and store city in cities and character sequence in pattern variable */
+		System.out.println("enter cities name separated with white space ");
+		//kathmandu lalitpur bhaktpur pokhara
+		cities=sc.nextLine();
+
+		System.out.println("enter pattern: ");
+		pattern=sc.next();
+		cities=cities.concat(" ");
+
+		/* pattern finding using loop detecting whitespace*/		
+		for (int i=0;i<cities.length();i++){
+
+			/* if case for separating each city and matching pattern*/
+			if (cities.charAt(i)==' '){
+				end=i;
+				String city=cities.substring(srt,end);
+				srt=i;
+
+				/* pattern matching*/
+				if (city.contains(pattern)){
+					System.out.println("city of pattern "+pattern+" is: "+city);	
+				}
+
+			/*outer if ends here*/
+			}
+		/*for loop ends here*/
+		}
+		
+				
+	/* main function end here*/	
+	}
+/* PatternDemo class ends here*/ 
+}
